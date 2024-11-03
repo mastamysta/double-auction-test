4ba0b3f38b07a7aa697e7a2d659aaf2bac89e6c2
mastamysta <benjamin-read@hotmail.co.uk>
2024-11-03 19:37:55 +0000

    Add skeleton code for testing of book class.

--- src/book.hpp
+++ src/book.hpp
@@ -1,6 +1,50 @@
 #pragma once
 
+#include <cstdint>
+#include <functional>
+
+using order_id = uint32_t;
+using order_size = uint32_t;
+using order_price = uint32_t;
+
 class book
 {
+public:
 
+    auto limit_buy(order_size, order_price) -> order_id;
+
+    auto limit_sell(order_size, order_price) -> order_id;
+
+    auto fok_buy(order_size, order_price) -> order_id; // UNSUPPORTED
+
+    auto fok_sell(order_size, order_price) -> order_id; // UNSUPPORTED
+
+    auto cancel_order(order_id id) -> void;
+
+    auto post_order_complete_callback(std::function<int(order_id, order_size, order_price)>) -> void;
 };
+
+auto book::limit_buy(order_size, order_price) -> order_id
+{
+    return 0;   
+}
+auto book::limit_sell(order_size, order_price) -> order_id
+{
+    return 0;   
+}
+auto book::fok_buy(order_size, order_price) -> order_id
+{
+    return 0;   
+}
+auto book::fok_sell(order_size, order_price) -> order_id
+{
+    return 0;   
+}
+auto book::cancel_order(order_id id) -> void
+{
+    return;   
+}
+auto book::post_order_complete_callback(std::function<int(order_id, order_size, order_price)>) -> void
+{
+    return;
+}
--- tests/testbook.cpp
+++ tests/testbook.cpp
@@ -1,12 +1,64 @@
 #include <iostream>
+#include <tuple>
 
 #include "gtest/gtest.h"
 
 #include "book.hpp"
 
-TEST(sample_test_case, sample_test)
+TEST(book_tests, can_instantiate)
 {
-    EXPECT_EQ(1, 1);
+    book b;
 }
 
+TEST(book_tests, can_buy)
+{
+    book b;
+
+    b.limit_buy(100, 100);
+}
+
+TEST(book_tests, can_sell)
+{
+    book b;
+
+    b.limit_sell(100, 100);
+}
+
+TEST(book_tests, can_match_buy_to_sell)
+{
+    book b;
+    std::vector<std::tuple<order_id, order_size, order_price>> cbs;
+    auto cb = [&](order_id id, order_size s, order_price p){
+        cbs.push_back({ id, s, p});
+        return 0;
+    };
+    b.post_order_complete_callback(std::function(cb));
+
+    auto sid = b.limit_sell(100, 100);
+    auto bid = b.limit_buy(100, 100);
+
+
+    EXPECT_TRUE(std::get<0>(cbs[0]) == sid || std::get<0>(cbs[0]) == bid);
+    EXPECT_TRUE(std::get<0>(cbs[1]) == sid || std::get<0>(cbs[1]) == bid);
+    EXPECT_NE(std::get<0>(cbs[0]), std::get<0>(cbs[1]));
+}
+
+TEST(book_tests, can_match_sell_to_buy)
+{
+    book b;
+    std::vector<std::tuple<order_id, order_size, order_price>> cbs;
+    auto cb = [&](order_id id, order_size s, order_price p){
+        cbs.push_back({ id, s, p});
+        return 0;
+    };
+    b.post_order_complete_callback(std::function(cb));
+
+    auto sid = b.limit_buy(100, 100);
+    auto bid = b.limit_sell(100, 100);
+
+
+    EXPECT_TRUE(std::get<0>(cbs[0]) == sid || std::get<0>(cbs[0]) == bid);
+    EXPECT_TRUE(std::get<0>(cbs[1]) == sid || std::get<0>(cbs[1]) == bid);
+    EXPECT_NE(std::get<0>(cbs[0]), std::get<0>(cbs[1]));
+}
 
